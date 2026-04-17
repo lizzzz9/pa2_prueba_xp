788bc0688f28a749b992ad8c60bebacdcbb53be1
Lizzpulupaxz <lizlizzpulupa9@gmail.com>
2026-04-17 01:09:47 -0500

    taller 4 sin quarkus

---
+++ taller4_java_sin_quarkus/NotificadorWhatsapp.java
@@ -0,0 +1,10 @@
+package com.edu.uce.negocio;
+
+public class NotificadorWhatsapp {
+
+    public void enviar (String numero,String mensaje ){
+        System.out.println("Se envia el Whatsapp al numero: "+numero);
+        System.out.println("Con el mensaje: "+mensaje);
+    }
+
+}
