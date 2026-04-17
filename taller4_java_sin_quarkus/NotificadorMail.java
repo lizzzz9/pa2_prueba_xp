91072e1627cceb6f0a1c3e6a5a81decc3aede324
Lizzpulupaxz <lizlizzpulupa9@gmail.com>
2026-04-17 01:08:21 -0500

    taller 4 sin quarkus

---
+++ taller4_java_sin_quarkus/NotificadorMail.java
@@ -0,0 +1,10 @@
+package com.edu.uce.negocio;
+
+public class NotificadorMail {
+
+    public void enviar (String correo,String mensaje ){
+        System.out.println("Se envia el email al correo: "+correo);
+        System.out.println("Con el mensaje: "+mensaje);
+    }
+
+}
